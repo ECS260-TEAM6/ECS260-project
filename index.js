0e350638fd38bc08cf4463832d5f44df63e146c3
uilnauyisDP <sssiu@ucdavis.edu>
2021-11-29 22:29:08 +1300

    Extract travis-enabled projects

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require('axios');
 var args = process.argv.slice(2);
 const fPath = args[0];
 const outFile = args[1];
+const task = args[2];
 
 async function processLineByLine() {
     const rStream = fs.createReadStream(fPath);
@@ -15,35 +16,10 @@ async function processLineByLine() {
     });
 
     for await (const line of rl) {
-        await sleep(100);
-
-        console.log(`Line from file: ${line}`);
-
-        // [login, project, language, watchers]
-        const items = line.split(',');
-        const login = items[0];
-        const project = items[1];
-        let useTravis = false;
-        let moreThanFiftyBuilds = false;
-
-        let res;
-        try {
-            const req = `https://api.travis-ci.org/repos/${login}/${project}`;
-            console.log(`Make request to ${req}`)
-            const res = await axios.get(`${req}`);
-            const data = res.data;
-
-            // check if 'last_build_number' is valid
-            if (typeof data.last_build_number == 'string') {
-                items.push(data.last_build_number);
-            } else {
-                items.push(-1);
-            }
-            await fs.appendFileSync(outFile, `${items.toString()}\n`);
-        } catch (e) {
-            console.error(e);
-            items.push(-1);
-            await fs.appendFileSync(outFile, `${items.toString()}\n`);
+        if (task == 'poke') {
+            await poke(line);
+        } else if (task == 'extract') {
+            await extract(line);
         }
     }
 }
@@ -51,5 +27,54 @@ async function processLineByLine() {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+async function poke(line) {
+    await sleep(100);
+
+    console.log(`Line from file: ${line}`);
+
+    // [login, project, language, watchers]
+    const items = line.split(',');
+    const login = items[0];
+    const project = items[1];
+    let useTravis = false;
+    let moreThanFiftyBuilds = false;
+
+    let res;
+    try {
+        const req = `https://api.travis-ci.org/repos/${login}/${project}`;
+        console.log(`Make request to ${req}`)
+        const res = await axios.get(`${req}`);
+        const data = res.data;
+
+        // check if 'last_build_number' is valid
+        if (typeof data.last_build_number == 'string') {
+            items.push(data.last_build_number);
+        } else {
+            items.push(-1);
+        }
+        await fs.appendFileSync(outFile, `${items.toString()}\n`);
+    } catch (e) {
+        console.error(e);
+        items.push(-1);
+        await fs.appendFileSync(outFile, `${items.toString()}\n`);
+    }
+}
+
+async function extract(line) {
+    console.log(`Line from file: ${line}`);
+
+    // [login, project, language, watchers]
+    const items = line.split(',');
+    const login = items[0];
+    const project = items[1];
+    const isEnabled = items[4];
+
+    if (isEnabled == '-1') {
+        return;
+    }
+    await fs.appendFileSync(outFile, `${items.slice(0, 2).join(' ')}\n`);
+}
+
 
 processLineByLine();
