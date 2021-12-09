dc23b19133d065d5998b4f9db124130f018f4096
Yuxuan Chen <chenyuxuang@gmail.com>
2021-12-08 18:30:22 -0800

    change plot

--- plot_travis_ci.py
+++ plot_travis_ci.py
@@ -3,8 +3,10 @@ import numpy as np
 import csv
 
 language_dict = {'Total': [0, 0, 0]}
+selected_languages = ['Total', 'Java', 'Python', 'Ruby']
 
-with open('gh-active-projects-legacy-o.csv') as csv_file:
+filename = 'repos_2021_500_out.csv'
+with open(filename) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
@@ -25,7 +27,10 @@ with open('gh-active-projects-legacy-o.csv') as csv_file:
 
 
 language_list = list(language_dict.items())
+# language_list = list(filter(lambda item: item[0] != '\\N', language_list))
+language_list = list(filter(lambda item: item[0] in selected_languages, language_list))
 language_list.sort(key=lambda e: sum(e[1]))
+# language_list = language_list[-6:]
 
 sorted_language = np.array(language_list, dtype=object)
 x = sorted_language[:, 0]
@@ -33,7 +38,6 @@ y = np.array([k for k in sorted_language[:, 1]])
 no_travis = y[:, 0]
 lte_travis = y[:, 1]
 gt_travis = y[:, 2]
-print(sorted_language)
 
 plt.barh(x, no_travis, label='Does Not Use Travis', color='tomato')
 plt.barh(x, lte_travis, left=no_travis, label='Uses Travis (<= 50 builds)', color='seagreen')
@@ -43,5 +47,4 @@ plt.ylabel('Main Repository Language')
 plt.xlabel('#Projects')
 plt.title('TRAVIS CI adoption per language')
 plt.legend()
-# plt.show()
-plt.savefig('travis_ci.png')
+plt.savefig('travis_ci_' + filename.split('.')[0] + '.png')
